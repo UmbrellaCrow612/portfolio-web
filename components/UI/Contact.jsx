f393ced155c8d40623ba78b7a9f6d6be7c7a91c7
yousaf wazir <umbrellaman612@gmail.com>
2022-10-23 17:50:30 +0100

    added contact

--- components/UI/Contact.jsx
+++ components/UI/Contact.jsx
@@ -12,11 +12,10 @@ const Contact = () => {
         <Row>
           <Col lg="6" md="6">
             <SectionSubtitle subtitle="Contact me" />
-            <h3 className="mt-4 mb-4">Contact with me</h3>
+            <h3 className="mt-4 mb-4">Contact me</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              beatae ratione iusto cum. Doloremque suscipit eius provident error
-              labore est reprehenderit, exercitationem corrupti earum deleniti!
+              Below are my forms of contact and ways to reach out to me,
+              un-fortunately the messaging form on the right does not work yet.
             </p>
 
             <ul className={`${classes.contact__info__list}`}>
@@ -24,37 +23,25 @@ const Contact = () => {
                 <span>
                   <i className="ri-map-pin-line"></i>
                 </span>
-                <p>Sylhet - Bangladesh</p>
+                <p>England - Sheffield</p>
               </li>
               <li className={`${classes.info__item}`}>
                 <span>
                   <i className="ri-mail-line"></i>
                 </span>
-                <p>muhib5532@gmail.com</p>
+                <p>umbrellaman612@gmail.com</p>
               </li>
               <li className={`${classes.info__item}`}>
                 <span>
                   <i className="ri-phone-line"></i>
                 </span>
-                <p>+088123456789</p>
+                <p>07459030947</p>
               </li>
             </ul>
 
             <div className={`${classes.social__links}`}>
-              <Link href="#">
-                <i className="ri-youtube-line"></i>
-              </Link>
-              <Link href="#">
+              <Link href="https://github.com/UmbrellaCrow612">
                 <i className="ri-github-line"></i>
-              </Link>
-              <Link href="#">
-                <i className="ri-facebook-line"></i>
-              </Link>
-              <Link href="#">
-                <i className="ri-linkedin-line"></i>
-              </Link>
-              <Link href="#">
-                <i className="ri-instagram-line"></i>
               </Link>
             </div>
           </Col>
